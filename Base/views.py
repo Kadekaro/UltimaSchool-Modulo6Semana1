c4c0a01467b47a1060738b3f2ba2a47d1312ecd8
kadekaro <kadekaroshop@gmail.com>
2023-12-19 18:35:21 -0300

    first commit

---
+++ Base/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def inicio(request):
+    return render(request, "inicio.html")
+
+
+def contato(request):
+    return render(request, "formulario.html")
